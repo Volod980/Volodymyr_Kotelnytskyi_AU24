694b46cd849f92a5b3328ed9dce220d334abda81
Volod980 <162060504+Volod980@users.noreply.github.com>
2024-11-05 20:57:28 +0200

    Add files via upload

--- SQL_SELECT_VOLODYMYR_KOTELNYTSKYI_HW/Part 2, task 1.sql
+++ SQL_SELECT_VOLODYMYR_KOTELNYTSKYI_HW/Part 2, task 1.sql
@@ -1,31 +1,26 @@
-with last_store as (
-				    select r.staff_id,
-				           inv.store_id,
-				           MAX(pm.payment_date) as last_payment_date
-				    from rental r
-				    left join payment pm 
-				    on pm.rental_id = r.rental_id
-				    left join public.inventory inv 
-				    on inv.inventory_id = r.inventory_id
-				    group by r.staff_id, inv.store_id
-),
-latest_store_where_staff_worked as (
-						    select distinct  on (ls.staff_id) 
-						           ls.staff_id,
-						           ls.store_id
+with last_store as ( select r.staff_id,
+		            inv.store_id,
+		            MAX(pm.payment_date) as last_payment_date
+		            from rental r
+			    left join payment pm 
+			    on pm.rental_id = r.rental_id
+			    left join public.inventory inv 
+			    on inv.inventory_id = r.inventory_id
+			    group by r.staff_id, inv.store_id),
+latest_store_where_staff_worked as (select distinct  on (ls.staff_id) 
+						        ls.staff_id,
+						        ls.store_id
 						    from last_store ls
 						    order by ls.staff_id, ls.last_payment_date desc ),
-amount as (
-		    select r.staff_id,
-		           sum(pm.amount) as total_amount
+amount as (select r.staff_id,
+		    sum(pm.amount) as total_amount
 		    from rental r
 		    inner join payment pm 
 		    on pm.rental_id = r.rental_id
 		    left join public.inventory inv 
 		    on inv.inventory_id = r.inventory_id
 		    where to_char(pm.payment_date,'dd.mm.yyyy') between '01.01.2017' and '31.12.2017'
-		    group by r.staff_id
-)
+		    group by r.staff_id)
 
 select lsp.staff_id,
        lsp.store_id,
@@ -38,7 +33,5 @@ where am.total_amount >= (
         select distinct total_amount
         from amount
         order by total_amount desc
-        LIMIT 3
-    ) as amn
-)
+        LIMIT 3) as amn)
 order by am.total_amount desc; 
