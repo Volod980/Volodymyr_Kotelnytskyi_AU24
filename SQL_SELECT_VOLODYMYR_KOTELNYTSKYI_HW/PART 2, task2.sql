70b215defa505ffa695b31b2bac3836a510486cd
Volod980 <162060504+Volod980@users.noreply.github.com>
2024-11-05 20:52:08 +0200

    Add files via upload

--- SQL_SELECT_VOLODYMYR_KOTELNYTSKYI_HW/PART 2, task2.sql
+++ SQL_SELECT_VOLODYMYR_KOTELNYTSKYI_HW/PART 2, task2.sql
@@ -1,11 +1,11 @@
 --Part 2, task 2  
    select title,
           count(r.rental_id) as num_of_rent,
-          case when f.rating = 'G' then '3+'
-	           when f.rating = 'PG' then '7+'
-               when f.rating = 'PG-13' then '13+'
-               when f.rating = 'R' then '17+'
-               when f.rating = 'NC-17' then '18+'
+          case when f.rating = 'G' then 'All ages admitted'
+	           when f.rating = 'PG' then 'Some material is not suitable for children'
+               when f.rating = 'PG-13' then 'under 13'
+               when f.rating = 'R' then 'Under 17'
+               when f.rating = 'NC-17' then 'Adults only'
                end expected_age
           from film f 
    left join  public.inventory inv
@@ -13,16 +13,16 @@
    inner join public.rental r 
    on inv.inventory_id = r.inventory_id 
    group by title,f.rating
-		   having count(rental_id) >= (select min(num_of_rent) 
-		                                         from  ( select title,
-							 count(r.rental_id) as num_of_rent
-							 from film f 
-							 left join  public.inventory inv
-							 on f.film_id = inv.film_id 
-							inner join public.rental r 
-							on inv.inventory_id = r.inventory_id 
-							group by title,f.rating
-							order by count(rental_id) desc
-							limit 5) v)
+  having count(rental_id) >= (select min(num_of_rent) 
+		                     from  ( select title,
+					            count(r.rental_id) as num_of_rent
+					            from film f 
+						    left join  public.inventory inv
+					            on f.film_id = inv.film_id 
+						    inner join public.rental r 
+						    on inv.inventory_id = r.inventory_id 
+						    group by title,f.rating
+						    order by count(rental_id) desc
+						    limit 5) v)
 	order by count(rental_id) desc			
-	--I have no film rating system in my task description. Where can I find it ?
+	--I hope that this rating system  will be more correct
